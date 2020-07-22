1c71340206f165c38a233206635c22f0c04bf81a
Alexander Moiseyev <daklaktakpak@yandex.ru>
2020-07-22 21:54:02 +0500

    install all dependecies

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 
 const path = require('path');
+const users = require('./data/users.json');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.get('/users', (req, res) => {
+  console.log(res.send(users[req.query.users]));
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
