3296478192117e56d4598324bf782d3f32c308a5
Alexander Moiseyev <daklaktakpak@yandex.ru>
2020-07-23 22:07:19 +0500

    fix review comments

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -9,25 +9,23 @@ const getUsersData = (req, res) => {
   res.status(200).send(users);
 };
 const searchUser = (usersArray, id) => {
-  const askedUser = usersArray.findIndex((user) => user._id === id);
+  const askedUser = usersArray.find((user) => user._id === id);
   return askedUser;
 };
 const doesUserExist = (req, res, next) => {
   const { id } = req.params;
   const askedUser = searchUser(users, id);
-  if (!users[askedUser]) {
-    res.status(404);
-    res.send({ message: 'Нет пользователя с таким id' });
+  if (!askedUser) {
+    res.status(404).send({ message: 'Нет пользователя с таким id' });
     return;
   }
   next();
 };
 
-const sendUser = (req, res, next) => {
+const sendUser = (req, res) => {
   const { id } = req.params;
   const askedUser = searchUser(users, id);
-  res.status(200).send(users[askedUser]);
-  next();
+  res.status(200).send(askedUser);
 };
 
 module.exports = {
